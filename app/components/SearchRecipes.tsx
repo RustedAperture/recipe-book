f3fb3ed0829d7dc88a975b2723bfc1794f158e13
Cameron Varley <cam.avarley@gmail.com>
2023-12-23 01:08:57 -0500

    api/search: rewrite function to async

--- app/components/SearchRecipes.tsx
+++ app/components/SearchRecipes.tsx
@@ -1,30 +1,31 @@
 "use client"
 
-import Link from "next/link"
+import { FormEvent } from 'react';
 import { useState } from "react"
 
 export default function SearchRecipes({getSearchResults}: any) {
     const [query, setQuery] = useState('');
 
     const onChange = async (e: any) => {
-        const q = e.target.value;
-        setQuery(q);
+        setQuery(e.target.value);
     }
 
     const onKeyUp = async (e: any) => {
-        handleSubmit(e);
+        onSubmit(e);
     }
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
         const response = await fetch(`/api/search?query=${query}`);
-        const recipe = await response.json();
-        getSearchResults(recipe)
+        const data = await response.json();
+        
+        getSearchResults(data)
     }
 
     return (
         <div className="flex grow">
-            <form onSubmit={handleSubmit} className="flex grow gap-3">
+            <form onSubmit={onSubmit} className="flex grow gap-3">
                 <input className="bg-zinc-800 grow bg-transparent rounded-xl border-zinc-800" type="text" placeholder="Search" value={query} onChange={onChange} onKeyUp={onKeyUp}/>
                 <button className="bg-zinc-800 border border-zinc-800 p-2 rounded-xl" type="submit">Seach</button>
             </form>
